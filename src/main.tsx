68635d95b78c7dd4c6e21adb41a8be8cbfd3274d
Thrashy190 <diego456.dlm77@gmail.com>
2023-02-17 21:18:47 -0600

    add changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import Router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Thrashy">
+    <BrowserRouter basename="/Thrashy/">
       <Router />
     </BrowserRouter>
   </React.StrictMode>
