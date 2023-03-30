7e0205a86704ca780ccf26b9b1e53de3da4ac8ed
Thrashy190 <diego456.dlm77@gmail.com>
2023-03-30 14:21:27 -0600

    fix version problems

--- src/router.tsx
+++ src/router.tsx
@@ -1,24 +1,27 @@
-import React, { FC } from "react";
-import { useRoutes, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import React, {FC} from "react";
+import {
+	useRoutes,
+	useLocation
+} from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
 
 const PortFolio = React.lazy(() => import("./views/Portfolio"));
 const Links = React.lazy(() => import("./views/Links"));
 
 const Router: FC = () => {
-  const location = useLocation();
-  const routing = useRoutes([
-    { path: "/", element: <Links /> },
-    { path: "Home", element: <PortFolio /> },
-  ]);
+	const location = useLocation();
+	const routing = useRoutes([
+		{path: "/", element: <Links/>},
+		{path: "Home", element: <PortFolio/>},
+	]);
 
-  if (!routing) return null;
+	if (!routing) return null;
 
-  return (
-    <AnimatePresence mode="wait" initial={false}>
-      {React.cloneElement(routing, { key: location.pathname })}
-    </AnimatePresence>
-  );
+	return (
+			<AnimatePresence mode="wait" initial={false}>
+				{React.cloneElement(routing, {key: location.pathname})}
+			</AnimatePresence>
+	);
 };
 
 export default Router;
